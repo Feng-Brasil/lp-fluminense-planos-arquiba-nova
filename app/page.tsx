8b299d3a8e775b2d4c2d9320a2812f79bbb9bae8
Matheus Andrade <63732320+matheusfeng@users.noreply.github.com>
2026-02-10 11:12:20 -0300

    Alterar ícone do terceiro box para cifrão ($) em branco e do quarto box para ícone de fogo em branco no primeiro scroll.

--- app/page.tsx
+++ app/page.tsx
@@ -112,7 +112,7 @@ export default function Home() {
             <div className="bg-black/60 backdrop-blur-md p-3 md:p-5 border-l-4 border-[#68121f] text-left">
               <div className="mb-4 text-white hidden md:block">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75m0 3v.75m0 3v.75m0 3V15m19.125-11.25h-15c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h15c.621 0 1.125-.504 1.125-1.125V5.625c0-.621-.504-1.125-1.125-1.125H3.375Z" />
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                 </svg>
               </div>
               <p className="text-[8px] md:text-xs font-bold uppercase tracking-wider leading-snug">Desconto garantido para todos os convidados</p>
@@ -120,7 +120,8 @@ export default function Home() {
             <div className="bg-black/60 backdrop-blur-md p-3 md:p-5 border-l-4 border-[#68121f] text-left">
               <div className="mb-4 text-white hidden md:block">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M15.362 5.214A8.252 8.252 0 0 1 12 21 8.25 8.25 0 0 1 6.038 7.047 8.287 8.287 0 0 0 9 9.601a8.983 8.983 0 0 1 3.361-6.867 8.21 8.21 0 0 1 3 2.48Z" />
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 18a3.75 3.75 0 0 0 .495-7.468 5.99 5.99 0 0 0-1.925 3.547 5.975 5.975 0 0 1-2.133-1.001A3.75 3.75 0 0 0 12 18Z" />
                 </svg>
               </div>
               <p className="text-[8px] md:text-xs font-bold uppercase tracking-wider leading-snug">Vamos todos juntos rumo à glória</p>

6dbbf723eba594c534827f4e25e35434f31f9659
Matheus Andrade <63732320+matheusfeng@users.noreply.github.com>
2026-03-12 15:30:25 -0300

    fix: No primeiro scroll, transformar o emoji de bola de...

--- app/page.tsx
+++ app/page.tsx
@@ -135,7 +135,10 @@ export default function Home() {
             </div>
             <div className="bg-black/60 backdrop-blur-md p-3 md:p-5 border-l-4 border-[#451021] text-left">
               <div className="mb-4 text-[rgb(248,248,248)] hidden md:block">
-                <span className="text-3xl">⚽</span>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M15.362 5.214A8.252 8.252 0 0 1 12 21 8.25 8.25 0 0 1 6.038 7.048 8.287 8.287 0 0 0 9 9.601a8.983 8.983 0 0 1 3.361-6.867 8.21 8.21 0 0 1 3 2.48Z" />
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 18a3.75 3.75 0 0 0 .495-7.467 5.99 5.99 0 0 0-1.925 3.546 5.974 5.974 0 0 1-2.133-1.401A3.75 3.75 0 0 0 12 18Z" />
+                </svg>
               </div>
               <p className="text-[8px] md:text-xs font-bold uppercase tracking-wider leading-snug text-[rgb(248,248,248)]">Vamos todos juntos rumo à glória</p>
             </div>
